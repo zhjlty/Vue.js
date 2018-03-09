4d23a99cb7a33ea71d3a4226a70a6e99d0e6215d
zhuhuijun <724536439@qq.com>
2018-03-09 12:13:23 +0800

    点赞

--- test3/test3.js
+++ test3/test3.js
@@ -1,14 +1,21 @@
 Vue.component('like',{
-  template:'<button @click="toggle_like()">👍{{like_count}}</button>',
+  template:'<button :class={liked:liked} @click="toggle_like()">👍{{like_count}}</button>',
   data: function(){
     return {
       like_count:10,
+      liked:false,
     }
   },
 
   methods:{
     toggle_like:function(){
-      this.like_count++;
+      if(!this.liked){
+        this.like_count++;
+      }
+      else{
+        this.like_count--;
+      }
+        this.liked = !this.liked;
     }
   }
 })
