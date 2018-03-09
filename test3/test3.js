e07d3877c2a223dd945ca0f4981ede815b416a3b
zhuhuijun <724536439@qq.com>
2018-03-09 12:46:23 +0800

    点赞

--- test3/test3.js
+++ test3/test3.js
@@ -1,25 +1,54 @@
-Vue.component('like',{
-  template:'<button :class={liked:liked} @click="toggle_like()">👍{{like_count}}</button>',
-  data: function(){
+// Vue.component('like',{
+//   template:'<button :class={liked:liked} @click="toggle_like()">👍{{like_count}}</button>',
+//   data: function(){
+//     return {
+//       like_count:10,
+//       liked:false,
+//     }
+//   },
+//
+//   methods:{
+//     toggle_like:function(){
+//       if(!this.liked){
+//         this.like_count++;
+//       }
+//       else{
+//         this.like_count--;
+//       }
+//         this.liked = !this.liked;
+//     }
+//   }
+// })
+//
+// new Vue({
+//   el: '#app',
+// })
+
+
+var liked_component = {
+  template:'<button :class={liked:liked} @click="toogle_like">👍{{like_count}}</button>',
+  data:function(){
     return {
-      like_count:10,
-      liked:false,
+      like_count:9,
+      liked:false
     }
   },
 
   methods:{
-    toggle_like:function(){
+    toogle_like:function(){
       if(!this.liked){
         this.like_count++;
       }
       else{
         this.like_count--;
       }
-        this.liked = !this.liked;
+      this.liked = !this.liked;
     }
   }
-})
-
+}
 new Vue({
-  el: '#app',
+  el:'#app',
+  components:{
+    like:liked_component
+  }
 })
